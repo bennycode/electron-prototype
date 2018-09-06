3d28b3b43a1ace104b8e1c6cb0287f24e7f58f24
Benny Neugebauer <bn@bennyn.de>
2018-09-06 17:48:47 +0200

    Remember window state

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,23 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const windowStateKeeper = require('electron-window-state');
 
 app.on('ready', () => {
-  const win = new BrowserWindow({width: 800, height: 600});
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600
+  });
+
+  const win = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height
+  });
+
+  mainWindowState.manage(win);
+
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
